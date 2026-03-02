d43376b41497450cb22b18f84e8d6d124fb6f36d
MhZ9191 <hegic91@gmail.com>
2026-03-02 14:54:45 +0100

    Add Show routing

--- routers/posts.js
+++ routers/posts.js
@@ -2,9 +2,31 @@ const postsData = require("../data/blogsData");
 const express = require("express");
 const posts = express();
 
+//! Index
 posts.get("/", (req, res) => {
   res.json({
+    message: "Visualizza tutti gli elementi",
     result: postsData,
+    success: true,
+  });
+});
+
+//! Show
+posts.get("/:id", (req, res) => {
+  const searcheditem = postsData.find(
+    (el) => el.id === parseInt(req.params.id),
+  );
+
+  if (!searcheditem) {
+    return res.status(404).json({
+      message: "elemento non trovato",
+      success: false,
+    });
+  }
+
+  res.json({
+    message: "Visualizza un singolo elemento",
+    result: searcheditem,
     success: true,
   });
 });
