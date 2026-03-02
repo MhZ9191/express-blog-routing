194321a94bce312a4e12959ddbaa4176fc74673a
MhZ9191 <hegic91@gmail.com>
2026-03-02 15:34:32 +0100

    Add other routes

--- routers/posts.js
+++ routers/posts.js
@@ -31,4 +31,51 @@ posts.get("/:id", (req, res) => {
   });
 });
 
+//! Store
+posts.post("/", (req, res) => {
+  res.json({
+    message: "Creo un nuovo elemento",
+    success: true,
+  });
+});
+
+//! Update
+posts.put("/:id", (req, res) => {
+  res.json({
+    message: "Modifico interamente un elemento",
+    success: true,
+  });
+});
+
+//! Modify
+posts.patch("/:id", (req, res) => {
+  res.json({
+    message: "Modifico parzialmente un elemento",
+    success: true,
+  });
+});
+
+//! Destroy
+posts.delete("/:id", (req, res) => {
+  const indexitem = postsData.findIndex(
+    (el) => el.id === parseInt(req.params.id),
+  );
+
+  if (indexitem === -1) {
+    return res.status(404).json({
+      message: "Elemento non trovato",
+      success: false,
+    });
+  }
+
+  const tmpElement = postsData[indexitem];
+  postsData.splice(indexitem, 1);
+  console.log(postsData);
+  res.json({
+    messagge: "Elemento eliminato",
+    result: tmpElement,
+    success: true,
+  });
+});
+
 module.exports = posts;
